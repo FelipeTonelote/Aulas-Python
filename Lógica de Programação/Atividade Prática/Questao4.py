8a9ad3e12b955a68674c01b304135493c65862df
Felipe <felipe.tonelote@hotmail.com>
2023-07-12 23:44:17 -0300

    alterando Questao4

--- Lógica de Programação/Atividade Prática/Questao4.py
+++ Lógica de Programação/Atividade Prática/Questao4.py
@@ -1,11 +1,9 @@
-# ------Inicio das Variaveis Globais-----
+# ------Inicio das Variáveis Globais-----
 lista_colaboradores = []
 id_global = 0
-# ------Fim das Variaveis Globais-----
+# ------Fim das Variáveis Globais-----
 
 # ------Inicio de cadastrar_colaborador(id)-----
-
-
 def cadastrar_colaborador(id):
     print('*' * 64, '\n' +
           '-' * 18, 'MENU CADASTRAR COLABORADOR', '-' * 18)
@@ -18,7 +16,6 @@ def cadastrar_colaborador(id):
                               'setor': setor,
                               'pagamento': pagamento}
     lista_colaboradores.append(dicionario_colaborador.copy())
-    print('*' * 64)
 # ------Fim de cadastrar_colaborador(id)-----
 
 # ------Inicio de consultar_colaborador()-----
@@ -32,36 +29,35 @@ def consultar_colaborador():
                                 '1 - Consultar TODOS os Colaboradores\n' +
                                 '2 - Consultar Colaborador por ID\n' +
                                 '3 - Consultar Colaborador(es) por setor\n' +
-                                '4 - Retornar\n' +
+                                '4 - Retornar ao Menu\n' +
                                 '>>')
         if opcao_consultar == '1':
             print('Você escolheu a opção de consultar TODOS os colaboradores')
             for colaborador in lista_colaboradores:  # colaborador vai varrer toda a lista de colaboradores
-                print('-' * 15)
+                print('-' * 64)
                 for key, value in colaborador.items():  # varrer todos os conjuntos de chave e valor do dicionario colaborador
                     print('{}: {}'.format(key, value))
-                print('-' * 15)
+                print('-' * 64)
         elif opcao_consultar == '2':
             print('Você escolheu a opção de consultar o colaborador por ID')
-            valor_desejado = int(
-                input('Digite o ID do colaborador desejado: '))
+            valor_desejado = int(input('Digite o ID do colaborador desejado: '))
             for colaborador in lista_colaboradores:
-                # o valor do campo ID desse dicionario é igual o id desejado?
+                # o valor do campo ID desse dicionario é igual o id desejado? Se sim, continua...
                 if colaborador['id'] == valor_desejado:
-                    print('-' * 15)
+                    print('-' * 64)
                     for key, value in colaborador.items():  # varrer todos os conjuntos de chave e valor do dicionario colaborador
                         print('{}: {}'.format(key, value))
-                    print('-' * 15)
+                    print('-' * 64)
         elif opcao_consultar == '3':
             print('Você escolheu a opção de consultar os colaborador(es) por setor')
             valor_desejado = input('Digite o setor do colaborador desejado: ')
             for colaborador in lista_colaboradores:
                 # o valor do campo setor desse dicionario é igual o setor desejado?
                 if colaborador['setor'] == valor_desejado:
-                    print('-' * 15)
+                    print('-' * 64)
                     for key, value in colaborador.items():  # varrer todos os conjuntos de chave e valor do dicionario colaborador
                         print('{}: {}'.format(key, value))
-                    print('-' * 15)
+                    print('-' * 64)
         elif opcao_consultar == '4':
             return  # sai da funcao consultar_colaborador() e volta para MAIN
         else:
@@ -79,22 +75,22 @@ def remover_colaborador():
         input('Digite o ID do colaborador que deseja remover: '))
     for colaborador in lista_colaboradores:
         if colaborador['id'] == valor_desejado:
-            lista_colaboradores.remove(colaborador)
+            lista_colaboradores.remove(colaborador) #remove o colaborador da lista
             print('Colaborador Removido!')
 
 # ------Fim de remover_colaborador()-----
 
 
 # ------Inicio da MAIN -----
-print('Bem-vindo ao Controle de Colaboradores de Felipe Beluci Tonelote\n' +
-      '*' * 64)
+print('Bem-vindo ao Controle de Colaboradores de Felipe Beluci Tonelote') #Identificação do proprietário código
 while True:
-    print('-' * 24, 'MENU PRINCIPAL', '-' * 24)
+    print('*' * 64, '\n' +
+          '-' * 24, 'MENU PRINCIPAL', '-' * 24)
     opcao_principal = input('Escolha a opção desejada:\n' +
                             '1 - Cadastrar Colaborador\n' +
                             '2 - Consultar Colaborador\n' +
                             '3 - Remover Colaborador\n' +
-                            '4 - Sair\n' +
+                            '4 - Encerrar programa\n' +
                             '>>')
     if opcao_principal == '1':
         id_global += 1
